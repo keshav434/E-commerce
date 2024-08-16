a8dfa4b847f504c4e698819f497a382f921824c7
keshav434 <premsagarkeshav@gmail.com>
2024-08-16 10:47:28 +0100

    Updated
    ddd

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require("express");
+const mongoose=require('mongoose');
+const app = express();
+const connectDB =async ()=>{
+    mongoose.connect("mongodb://localhost:27017/e-comm");
+    const productSchema= new mongoose.Schema({});
+    const product= mongoose.model("products",productSchema);
+    const data = await product.find();
+    console.warn(data);
+}
+
+connectDB();
+app.listen(4000);
